82ffafd8b29bf0cfe0f91b3058c74855ca2689df
walterexer <walterexer.9@gmail.com>
2026-01-07 03:58:16 +0000

    Update telegram-hook.ts

--- api/telegram-hook.ts
+++ api/telegram-hook.ts
@@ -13,7 +13,7 @@ const bot = new Telegraf(BOT_TOKEN);
 export async function handleStartCommand(ctx) {
   const COMMAND = "/start";
   const channelUrl = "t.me/faasbsda008324";
-  const targetUrl = "t.me/+YCGqgBtN_flkNGU0";
+  const targetUrl = "t.me/+W5VZUM6EExoxMGY0";
 
   // Welcome message with Markdown formatting
   const reply = `
