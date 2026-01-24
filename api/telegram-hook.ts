9c35c860c0231f5d108516a82c5fcd5e69313889
walterexer <walterexer.9@gmail.com>
2026-01-24 14:59:50 +0000

    Change target URL in handleStartCommand
    Updated target URL in handleStartCommand function.

--- api/telegram-hook.ts
+++ api/telegram-hook.ts
@@ -13,7 +13,7 @@ const bot = new Telegraf(BOT_TOKEN);
 export async function handleStartCommand(ctx) {
   const COMMAND = "/start";
   const channelUrl = "t.me/fastmproxies";
-  const targetUrl = "t.me/+gg1Xz9nBksA4Mjg8";
+  const targetUrl = "t.me/+sYDr1n06KD1mZTRk";
 
   // Welcome message with Markdown formatting
   const reply = `
